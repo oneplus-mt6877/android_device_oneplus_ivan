8d2e578b5961d807b1c047b96269ee167c15043f
Alexander Koskovich <akoskovich@pm.me>
2022-12-15 19:40:02 +0000

    ivan: Move BT profiles props to vendor  * Makes it GSI compatible
    Change-Id: I8f7caf241d508af59c16482e6185c42f23f8a460

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,6 @@ TARGET_BOARD_PLATFORM := mt6877
 BOARD_HAS_MTK_HARDWARE := true
 
 # Properties
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
