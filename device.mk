c42a0ad517058eb376af48eb4e2c519e15fc2251
Alone0316 <rutujamandlik06@gmail.com>
2023-12-06 12:11:14 +0000

    ivan: Build vibrator stack from source

--- device.mk
+++ device.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.mtk \
     android.hardware.thermal@1.0-impl
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.mediatek
+
 # WiFi
 PRODUCT_PACKAGES += \
     wpa_supplicant \
