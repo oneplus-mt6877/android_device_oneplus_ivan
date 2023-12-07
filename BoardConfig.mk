1b6c0cf04bdc256804544c5e2b600cdb14dea95b
Alone0316 <rutujamandlik06@gmail.com>
2023-12-07 18:31:24 +0000

    ivan: Import compatibility_matrix.xml from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
