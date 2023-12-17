9e075c42da20f100b4b2cada37d821470068324a
Alone0316 <rutujamandlik06@gmail.com>
2023-12-17 12:17:30 +0000

    ivan: Generate Framework compatibility matrix * Made with https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
